988c9bd928f18fa53ad73625f29378f77051ac60
FarOutWest <westDcastro@gmail.com>
2018-04-08 21:38:56 -0600

    updates to drmmessages.py - although it is correct for the test casees it does not pass the third to last case on kattis with response 'wrong' answer. need to look into edge cases for letter shifts

--- drmmessages.py
+++ drmmessages.py
@@ -1,10 +1,34 @@
 #! usr/bin/env Python3
+import math
 msg = input()
 alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 parta, partb = msg[:int(len(msg)/2)], msg[int(len(msg)/2):]
 suma, sumb = 0, 0
+for ch in parta: suma += alph.index(ch)
+for ch in partb: sumb += alph.index(ch)
+totshift = 0
+newa = []
+newb = []
+ans = []
 for ch in parta:
-    suma += alph.index(ch)
+    totshift = suma + alph.index(ch)
+    if totshift >= 26:
+        subs = math.floor(totshift / 26)
+        totshift -= (subs * 26)
+        newa.append(alph[totshift])
 for ch in partb:
-    sumb += alph.index(ch)
-#shift function to shift each letter based on the sumvals
+    totshift = sumb + alph.index(ch)
+    if totshift >= 26:
+        subs = math.floor(totshift / 26)
+        totshift -= (subs * 26)
+        newb.append(alph[totshift])
+newa = ''.join(newa)
+newb = ''.join(newb)
+for i in range(len(newa)):
+    shift = alph.index(newb[i]) + alph.index(newa[i])
+    if shift >= 26:
+        subs = math.floor(shift / 26)
+        shift -= (subs * 26)
+        ans.append(alph[shift])
+    else: ans.append(alph[shift])
+print(''.join(ans))
