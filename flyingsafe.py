8f79287dd0d46524ffd3e05c9f4a5d9e1d6cc4f3
FarOutWest <westDcastro@gmail.com>
2018-04-25 22:17:52 -0600

    updates to ans

---
+++ flyingsafe.py
@@ -0,0 +1,8 @@
+#! usr/bin/env Python3
+for i in range(int(input())):
+    case = [int(a) for a in input().split()]
+    cities, pilots = case[0], case[1]
+    for j in range(pilots):
+        ab = [int(a) for a in input().split()]
+        citya, cityb = ab[0], ab[1]
+    print(cities-1)
