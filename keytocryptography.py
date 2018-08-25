613110153ee4ba797cd8d17362cfaef333e7cb37
FarOutWest <westDcastro@gmail.com>
2018-08-24 18:34:53 -0600

    updated solutions

---
+++ keytocryptography.py
@@ -0,0 +1,21 @@
+cypher = [ch for ch in input().strip()]
+secret = [ch for ch in input().strip()]
+og_secret = secret
+if len(secret) > len(cypher):
+    for i in range(len(secret) - len(cypher)): secret.pop()
+    og_secret = secret
+shifts = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6,
+          'H':7, 'I':8, 'J':9, 'K':10, 'L':11, 'M':12, 'N':13,
+          'O':14, 'P':15, 'Q':16, 'R':17, 'S':18, 'T':19,'U':20,
+          'V':21, 'W':22, 'X':23, 'Y':24, 'Z':25}
+shiftnums, cyphernums, ans = [], [], []
+for ch in cypher: cyphernums.append(shifts.get(ch))
+while len(ans) < len(cypher):
+    secret = og_secret + ans
+    shiftnums, ans = [], []
+    for ch in secret: shiftnums.append(shifts.get(ch))
+    for i in range(len(secret)):
+        shift = cyphernums[i] - shiftnums[i]
+        if shift < 0: shift = 26 + shift
+        ans.append(list(shifts.keys())[list(shifts.values()).index(shift)])
+print (''.join(ans))
--- synclists.py
+++ synclists.py
@@ -1,16 +1,15 @@
 #! usr/bin/env Python3
+def synchronize(l1, l2):
+    mapping = dict(zip(sorted(l1), sorted(l2)))
+    return [mapping[x] for x in l1]
+
 while True:
     n = int(input())
     if n != 0:
         listOne, listTwo = [], []
         for i in range(n): listOne.append(int(input()))
         for i in range(n): listTwo.append(int(input()))
-        print("listOne: ", listOne)
-        print("listTwo: ", listTwo)
-        #ALGO options:
-        #   find the rankings of listOne and listTwo
-        #   apply the rankings order of ListOne
-        #    to the contents of listTwo
-        for item in listOne:
-
+        ans = (synchronize(listOne, listTwo))
+        for i in range(len(ans)):
+            print(ans[i])
     else: break
